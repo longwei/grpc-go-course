edfa534e2142fd305ac91b872aedd737dc6d6384
Longwei Su <longwei.su@gmail.com>
2023-02-16 11:29:13 -0500

    boilerplate steup

---
+++ greet/server/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net"
+
+	pb "github.com/longwei/grpc-go-course/greet/proto"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	pb.GreetServiceServer
+}
+
+var addr string = "0.0.0.0:50051"
+
+func main() {
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatal("Failed to listen on: %v\n", err)
+	}
+
+	log.Printf("Listening on %s\n", addr)
+
+	s := grpc.NewServer()
+
+	if err = s.Serve(lis); err != nil {
+		log.Fatal("Failed to serv: %v\n", err)
+	}
+
+}
